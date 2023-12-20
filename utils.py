bc3c6771ebb52b17d4078d55b3776fe533a171f7
xinzipanghuang <46417821+xinzipanghuang@users.noreply.github.com>
2023-12-20 17:06:05 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from langchain.chat_models import tongyi,ChatOpenAI
 
-api_key="sk-77f54bfeb4eb4dda83e115e2687d8564"
+api_key=""
 LLM_MODEL = tongyi.ChatTongyi(dashscope_api_key=api_key,streaming=True)
 
 # openai_key=""
